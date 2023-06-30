d84c29ec772912f3027023c2f337ef8790667bf4
josaya <josaya@josaya.josaya>
2023-06-30 02:41:23 +0200

    committing make classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,5 +2,5 @@ import ClassRoom from "./0-classroom.js";
 
 function initializeRooms() {
     // Return an array of three ClassRoom objects with the sizes 19, 20, and 34
-  return ([new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]);
+    return ([new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]);
 }
