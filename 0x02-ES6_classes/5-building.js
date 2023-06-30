638680e68ede396ee3c128ede8823bd45b6b201b
josaya <josaya@josaya.josaya>
2023-06-30 04:05:28 +0200

    committing building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,13 +2,13 @@ export default class Building {
     constructor(sqft) {
         this._sqft = sqft;
         if (this.constructor !== Building && !this.evacuationWarningMessage) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
+            throw Error('Class extending Building must override evacuationWarningMessage');
         }
     }
 
     set sqft(sqft) {
         if (typeof sqft !== 'number') {
-        throw TypeError('sqft must be a number');
+            throw TypeError('sqft must be a number');
         }
         this._sqft = sqft;
     }
