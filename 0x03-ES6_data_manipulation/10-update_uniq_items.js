15e066f51fa925b3e9d9446cd90a862fe3fcc2f0
josaya <josaya@josaya.josaya>
2023-07-05 01:40:50 +0200

    comitting update uniq items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,19 @@
 export default function updateUniqueItems(map) {
+    // Check if the input is a Map instance
     if (!(map instanceof Map)) {
       throw new Error('Cannot process');
     }
   
+    // Iterate over the entries of the map
     for (const [key, value] of map.entries()) {
+      // Check if the value is equal to 1
       if (value === 1) {
+        // If the value is 1, update it to 100
         map.set(key, 100);
       }
     }
   
+    // Return the updated map
     return map;
   }
   
