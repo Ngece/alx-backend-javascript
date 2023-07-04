be3b4c69c6ed02c2a7a3cd6b300202b6157a9d73
josaya <josaya@josaya.josaya>
2023-07-05 00:03:00 +0200

    committing get list students id

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,6 @@
 export  default function getListStudentIds(array) {
     const arrayIds = [];
+    
     if (Array.isArray(array)) {
         array.forEach((element) => {
         arrayIds.push(element.id);
