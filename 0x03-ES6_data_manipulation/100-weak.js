6e16219cd750bd14e48d1ccb33271c22c7cc2c8a
josaya <josaya@josaya.josaya>
2023-07-05 01:03:18 +0200

    comitting weak

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,4 +1,10 @@
-export default function weakFromArray(array) {
-            
-    return new WeakSet(array);
-}
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  let total = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
+  return total;
+};
+
+export { weakMap, queryAPI };
