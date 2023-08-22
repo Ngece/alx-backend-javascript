bf43a009caa24580c410586182025cbee5534066
Ngece <ngecemlamli@gmail.com>
2023-08-23 00:19:25 +0200

    committing read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,38 +1,42 @@
-// Syncronously reads and prints the contents of a database file.
+// Syncronously reads content from a file and prints it to the stdout
 
 const fs = require('fs');
 
-// Counts the number of students in a database.
-function countStudents(path) { 
-    try {
-        const data = fs.readFileSync(path, 'utf8');
-        const lines = data.split('\n');
-        let count = lines.length;
-        if (lines[lines.length - 1] === '') {
-        count -= 1;
-        }
-        console.log(`Number of students: ${count}`);
-        const fields = {};
-        for (const line of lines) {
-        const student = line.split(',');
-        if (fields[student[3]] === undefined && student[3] !== 'field') {
-            fields[student[3]] = {
-            count: 1,
-            names: [student[0]],
-            };
-        } else if (student[3] !== 'field') {
-            fields[student[3]].count += 1;
-            fields[student[3]].names.push(student[0]);
-        }
-        }
-        for (const field of Object.keys(fields)) {
-        if (field) {
-            console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
-        }
-        }
-    } catch (err) {
-        throw Error('Cannot load the database');
-    }
-    };
+// Count the number of students in a given file
+function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+
+            const lines = data.split('\n').filter(line => line.trim() !== '');
+            const count = lines.length;
+            console.log(`Number of students: ${count}`);
+
+            const fields = {};
+            for (const line of lines) {
+                const student = line.split(',');
+                if (fields[student[3]] === undefined && student[3] !== 'field') {
+                    fields[student[3]] = {
+                        count: 1,
+                        names: [student[0]],
+                    };
+                } else if (student[3] !== 'field') {
+                    fields[student[3]].count += 1;
+                    fields[student[3]].names.push(student[0]);
+                }
+            }
+
+            for (const field of Object.keys(fields)) {
+                if (field) {
+                    console.log(`Number of students in ${field}: ${fields[field].count}. List: ${fields[field].names.join(', ')}`);
+                }
+            }
+            resolve();
+        });
+    });
+}
 
 module.exports = countStudents;
