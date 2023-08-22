e9ba56c06f63f1a704e96e7d0c351b37ea8fbe1f
Ngece <ngecemlamli@gmail.com>
2023-08-23 01:26:53 +0200

    committing 5 http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,47 +1,28 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const countStudents = require('./3-read_file_async');
+// http server with advanced features
 
 const hostname = '127.0.0.1';
 const port = 1245;
-const databasePath = 'database.csv'; 
+const http = require('http');
+const students = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    const { method, url } = req;
-
-    if (method === 'GET') {
-        if (url === '/') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Hello Holberton School!\n');
-        } else if (url === '/students') {
-            res.setHeader('Content-Type', 'text/plain');
-            
-            countStudents(databasePath)
-                .then(() => {
-                    res.statusCode = 200;
-                    const readStream = fs.createReadStream(path.resolve(__dirname, databasePath));
-                    readStream.pipe(res);
-                })
-                .catch((error) => {
-                    res.statusCode = 500;
-                    res.end(error.message);
-                });
-        } else {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('Not Found\n');
-        }
-    } else {
-        res.statusCode = 405;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Method Not Allowed\n');
-    }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
+  }
 });
-
+  
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
