b658daa757adfa3b5f1d2fd912fdd502811194bf
Ngece <ngecemlamli@gmail.com>
2023-08-23 01:02:53 +0200

    committing

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,10 +4,7 @@ const http = require('http');
 
 // Create an HTTP server
 const app = http.createServer((req, res) => {
-    // Set the response header
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    // Send the response body
     res.end('Hello Holberton School!\n');
 });
 
@@ -16,5 +13,4 @@ app.listen(1245, () => {
     console.log('Server is running on port 1245');
 });
 
-// Export the app variable
 module.exports = app;
