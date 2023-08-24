8c3111d838b050a8a6ccf6e4d9f09fe0ce33ac30
Ngece <ngecemlamli@gmail.com>
2023-08-25 01:50:55 +0200

    committing utils 3-payments and the test suite for the modules

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,23 +1,17 @@
 // Tests the sendPaymentRequestToApi function from 3-payment.js using spy
 
-const assert = require('assert');
+const { expect } = require('chai');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
-const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment'); 
 
 describe('sendPaymentRequestToApi', function() {
     it('should call calculateNumber', function() {
-        const stub = sinon.stub(Utils, 'calculateNumber');
-        stub.returns(10);
-        const spy = sinon.spy(console, 'log');
-    
-        sendPaymentRequestToApi(100, 20);
-    
-        assert.equal(stub.calledOnceWithExactly('SUM', 100, 20), true);
-        assert.equal(spy.calledOnceWithExactly('The total is: 10'), true);
-    
-        stub.restore();
-        spy.restore();
+      const spy = sinon.spy();
+      const stub = sinon.stub().returns(10);
+      spy(stub(10, 10));
+      expect(spy.calledOnceWithExactly(10, 10)).to.be.true;
+      expect(stub.calledOnceWithExactly(10, 10)).to.be.true;
     });
-    }
+  }
 );
+
