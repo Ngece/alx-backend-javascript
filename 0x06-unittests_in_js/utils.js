032a5d9a31b5b559bf97ad1c562a971f11dac1ae
Ngece <ngecemlamli@gmail.com>
2023-08-25 01:33:53 +0200

    committing utiles module

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,22 @@
+// Util module
+
+const Utils = {
+    calculateNumber(type, a, b) {
+      const aNum = Math.round(a);
+      const bNum = Math.round(b);
+      if (type === 'SUM') {
+        return aNum + bNum;
+      }
+      if (type === 'SUBTRACT') {
+        return aNum - bNum;
+      }
+      if (type === 'DIVIDE') {
+        if (bNum === 0) {
+          return 'Error';
+        }
+        return aNum / bNum;
+      }
+    },
+  };
+    
+  module.exports = Utils;
