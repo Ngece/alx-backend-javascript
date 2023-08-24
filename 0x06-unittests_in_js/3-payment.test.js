39cd0bf2c2adc0ae041b900e5a4f0769fb4387ed
Ngece <ngecemlamli@gmail.com>
2023-08-25 01:54:40 +0200

    committing utils 3-payments and the test suite for the modules

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,30 +1,17 @@
 // Tests the sendPaymentRequestToApi function from 3-payment.js using spy
 
-const assert = require('chai').assert;
-const expect = require('chai').expect;
-const chai = require('chai');
-const spies = require('chai-spies');
-
-chai.use(spies);
-
-const calculateNumber = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment'); 
 
 describe('sendPaymentRequestToApi', function() {
-  it('should calculate the total and log the result', function() {
-    const spy = chai.spy.on(console, 'log');
-
-    const totalAmount = 100;
-    const totalShipping = 10;
-    const expectedResult = totalAmount + totalShipping;
-
-    const result = sendPaymentRequestToApi(totalAmount, totalShipping);
-
-    expect(spy).to.have.been.called.with(`The total is: ${expectedResult}`);
-    assert.equal(result, expectedResult);
-
-    chai.spy.restore(console, 'log');
-  });
-});
-
+    it('should call calculateNumber', function() {
+      const spy = sinon.spy();
+      const stub = sinon.stub().returns(10);
+      spy(stub(10, 10));
+      expect(spy.calledOnceWithExactly(10, 10)).to.be.true;
+      expect(stub.calledOnceWithExactly(10, 10)).to.be.true;
+    });
+  }
+);
 
