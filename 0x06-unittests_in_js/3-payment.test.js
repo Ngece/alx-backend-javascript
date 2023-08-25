78dc5dbc2146b8f1706b85234807d61611630661
Ngece <ngecemlamli@gmail.com>
2023-08-25 02:04:04 +0200

    committing 3 payment test suite

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,16 +2,19 @@
 
 const { expect } = require('chai');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment'); 
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', function() {
     it('should call calculateNumber', function() {
-      const spy = sinon.spy();
-      const stub = sinon.stub().returns(10);
-      spy(stub(10, 10));
-      expect(spy.calledOnceWithExactly(10, 10)).to.be.true;
-      expect(stub.calledOnceWithExactly(10, 10)).to.be.true;
+        const spy = sinon.spy(Utils, 'calculateNumber');
+        const spyConsole = sinon.spy(console, 'log');
+        sendPaymentRequestToApi(100, 20);
+        expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+        spy.restore();
+        spyConsole.restore();
     });
-  }
+    }
 );
 
