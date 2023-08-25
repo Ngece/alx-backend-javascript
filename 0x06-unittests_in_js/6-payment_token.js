a61fed6ea1c2efe10a03bd36ba56352fe7ce88d4
Ngece <ngecemlamli@gmail.com>
2023-08-25 02:22:43 +0200

    committing 6 payment token module with test suite

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+// module for managing payment processing using tokens
+
+function getPaymentTokenFromAPI(success) {
+  if (success === true) {
+    return Promise.resolve({ data: 'Successful response from the API' });
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
